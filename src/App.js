d0847e11c72497c5d51b037ec93494ae8b6873ef
Ronak Mutha <42516870+Ronak-Mutha@users.noreply.github.com>
2021-11-21 20:50:42 +0530

    Refactor: changed setCount to take callback function

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
 import "./styles.css";
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 export default function App() {
-
-  const [ count, setCount ] = useState(1);
-  const [ color, setColor ] = useState('black');
-
+  const [count, setCount] = useState(1);
+  const [color, setColor] = useState("black");
+  
   useEffect(() => {
-    count % 15 === 0 ?
-      setColor('yellow') :
-    count % 5 === 0 ?
-    setColor('green') :
-    count % 3 === 0 ?
-    setColor('red') :
-    setColor('black');
+    count % 15 === 0
+      ? setColor("yellow")
+      : count % 5 === 0
+      ? setColor("green")
+      : count % 3 === 0
+      ? setColor("red")
+      : setColor("black");
   }, [count, color]);
 
   function fizzBuzz() {
-    setCount(count + 1); 
+    setCount((prevCount) => prevCount + 1);
   }
   return (
     <div className="App">
-      <p style={{color}}>{count}</p>
+      <p style={{ color }}>{count}</p>
       <button onClick={fizzBuzz}>Increment Count </button>
-   </div>
+    </div>
   );
 }
